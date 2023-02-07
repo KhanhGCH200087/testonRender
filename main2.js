252730b9883bfa0b0b3956a27c067b4ecfd3c585
KhanhGCH200087 <105761671+KhanhGCH200087@users.noreply.github.com>
2023-02-07 10:39:07 +0700

    Update main2.js

--- main2.js
+++ main2.js
@@ -4,13 +4,14 @@ var app = express()
 
 app.get('/',(req, res) => {
     res.writeHead(200, {'Content-Type' : 'text/html'});
-    res.write('<html><body><p>This is the homepage</p></body></html>');
+    res.write('<html><body><p>This is the homepage</p><a href="/student"><span>Product</span></a></body></html>');
     res.end(); //cần phải có end ko thì server sẽ chạy liên tục
 })
 
 app.get('/student',(req, res) => {
     res.writeHead(200, {'Content-Type' : 'text/html'});
-    res.write('<html><body><p>This is the student page</p></body></html>');
+     res.write('<html><body><p>This is the student page</p><a href="/"><span>Home</span></a></body></html>');
+    res.write('<html><body><img src="https://th.bing.com/th/id/OIP.ihgGf7X6RINzYF2F_nr7vQHaFj?w=247&h=185&c=7&r=0&o=5&dpr=1.3&pid=1.7" alt="Flowers in Chania" width="100" height="100"></body></html>');
     res.end(); //cần phải có end ko thì server sẽ chạy liên tục
 })
 
